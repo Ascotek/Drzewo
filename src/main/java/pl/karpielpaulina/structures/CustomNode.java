179970558b689fd05abd307c5904f4bda4cf24ea
Ascotek <paulinatochwin@gmail.com>
2019-05-11 10:27:40 +0200

    commit2

---
+++ src/main/java/pl/karpielpaulina/structures/CustomNode.java
@@ -0,0 +1,22 @@
+package pl.karpielpaulina.structures;
+
+public class CustomNode {
+    private String data;
+    private CustomNode next;
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    public CustomNode getNext() {
+        return next;
+    }
+
+    public void setNext(CustomNode next) {
+        this.next = next;
+    }
+}
